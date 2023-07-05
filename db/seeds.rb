e13b9b8408ea86f7fd30140c7e06708a84194955
samuel <samuelkelvinmwangi@gmail.com>
2023-07-05 16:44:04 +0300

    solving problem one of toyproblem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def compare_numbers(a, b)
+    diff = a - b
+    "   #{a} is #{'greater than' * (diff > 0 ? 1 : 0)}
+        #{'smaller than' * (diff < 0 ? 1 : 0)}
+        #{'equal to' * (diff == 0 ? 1 : 0)} 
+        #{b}"
+  end
+  
